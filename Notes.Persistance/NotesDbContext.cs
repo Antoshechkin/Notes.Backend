838b39289ee44be233373d4915a6f0017b4e662d
Antoshechkin <sasha.antoshechkin@mail.ru>
2024-03-21 16:45:31 +0300

    lesson 5

--- Notes.Persistance/NotesDbContext.cs
+++ Notes.Persistance/NotesDbContext.cs
@@ -7,7 +7,7 @@ namespace Notes.Persistance
 {
     public class NotesDbContext : DbContext, INotesDbContext
     {
-        public DbSet<Note> Notes {  get; set; }
+        public DbSet<Note> Notes { get; set; }
 
         public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
 
